5ef3f6d597faaf259180afcab98958bbc235b0b6
Joel Fonseca Nogueira <39314922+joelfnogueira@users.noreply.github.com>
2024-08-19 11:34:28 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sanger_qc",
+    version="0.1.0",
+    author="Your Name",
+    author_email="yourname@domain.com",
+    description="A Python script for converting .ab1 files to .fasta and .qual, and trimming sequences based on quality scores.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/sanger_qc",
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.18.0",
+        "biopython>=1.78"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'sanger_qc=sanger_qc:main',
+        ],
+    },
+)
